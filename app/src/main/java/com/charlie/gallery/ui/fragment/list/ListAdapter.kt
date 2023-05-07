43b090a5b5cdbcaeae8cd62c3d0d13f4be68ba04
Charlie_Moon <moon3482@cau.ac.kr>
2023-05-07 22:14:14 +0900

    #5 ListAdapter 수정
    imageData -> ImageItemData 변경

--- app/src/main/java/com/charlie/gallery/ui/fragment/list/ListAdapter.kt
+++ app/src/main/java/com/charlie/gallery/ui/fragment/list/ListAdapter.kt
@@ -2,30 +2,29 @@ package com.charlie.gallery.ui.fragment.list
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.charlie.gallery.model.ImageData
+import com.charlie.gallery.model.ImageItemData
 
 class ListAdapter(
-    private val imageDataList: MutableList<ImageData> = mutableListOf(),
+    private val imageItemDataList: MutableList<ImageItemData> = mutableListOf(),
     private val onClickViewHolder: (Int) -> Unit,
 ) : RecyclerView.Adapter<ListViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder =
         ListViewHolder(parent)
 
-    override fun getItemCount(): Int = imageDataList.size
+    override fun getItemCount(): Int = imageItemDataList.size
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        val data = imageDataList[position]
+        val data = imageItemDataList[position]
         holder.bind(
             data,
-            imageDataList[position].id,
-            onClickViewHolder
+            onClickViewHolder,
         )
     }
 
-    fun initList(imageDataList: List<ImageData>) {
-        this.imageDataList.clear()
-        this.imageDataList.addAll(imageDataList)
-        notifyItemRangeChanged(0, this.imageDataList.size)
+    fun initList(imageItemDataList: List<ImageItemData>) {
+        this.imageItemDataList.clear()
+        this.imageItemDataList.addAll(imageItemDataList)
+        notifyItemRangeChanged(0, this.imageItemDataList.size)
     }
 }
