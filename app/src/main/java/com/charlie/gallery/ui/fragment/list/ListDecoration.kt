e319da11163fdfe4d63408b0b37ef43eb1387bcb
Charlie_Moon <moon3482@cau.ac.kr>
2023-05-07 22:01:24 +0900

    #5 ListDecoration 수정

--- app/src/main/java/com/charlie/gallery/ui/fragment/list/ListDecoration.kt
+++ app/src/main/java/com/charlie/gallery/ui/fragment/list/ListDecoration.kt
@@ -2,12 +2,15 @@ package com.charlie.gallery.ui.fragment.list
 
 import android.graphics.Rect
 import android.view.View
+import androidx.annotation.Px
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 
-class ListDecoration : ItemDecoration() {
-
-    private val span: Int = 2
+class ListDecoration(
+    @Px private val width: Int,
+    @Px private val height: Int,
+) : ItemDecoration() {
 
     override fun getItemOffsets(
         outRect: Rect,
@@ -15,24 +18,37 @@ class ListDecoration : ItemDecoration() {
         parent: RecyclerView,
         state: RecyclerView.State,
     ) {
-        val itemCount: Int = state.itemCount
+        val spanCount = when (parent.layoutManager) {
+            is GridLayoutManager -> (parent.layoutManager as GridLayoutManager).spanCount
+            else -> 1
+        }
+        val spanIndex = when (parent.layoutManager) {
+            is GridLayoutManager -> (view.layoutParams as GridLayoutManager.LayoutParams).spanIndex
+            else -> 1
+        }
+        val itemCount = state.itemCount
         val position: Int = parent.getChildAdapterPosition(view)
 
-        if (position % span == 0) {
-            outRect.right = 5
-        } else {
-            outRect.left = 5
+        when (spanIndex) {
+            0 -> outRect.right = width / 2
+
+            spanCount - 1 -> outRect.left = width / 2
+
+            else -> {
+                outRect.left = width / 2
+                outRect.right = width / 2
+            }
         }
 
-        if (position < span) {
-            outRect.top = 0
-            outRect.bottom = 4
-        } else if (position >= itemCount - span) {
-            outRect.bottom = 0
-            outRect.top = 4
-        } else {
-            outRect.bottom = 4
-            outRect.top = 4
+        when {
+            position < spanCount -> outRect.bottom = height / 2
+            
+            (itemCount - spanCount) <= position -> outRect.top = height / 2
+
+            else -> {
+                outRect.bottom = height / 2
+                outRect.top = height / 2
+            }
         }
     }
 }
