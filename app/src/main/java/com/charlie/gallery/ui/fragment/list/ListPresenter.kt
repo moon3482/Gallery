aa953378a14f2ab50c356f2d598803d74953c65b
문창규 <ckmoon@smartscore.kr>
2023-05-12 11:02:13 +0900

    #7 ListPresenter 수정
    - start 함수 수정
    - ListContract.Presenter 추가 함수 구현

--- app/src/main/java/com/charlie/gallery/ui/fragment/list/ListPresenter.kt
+++ app/src/main/java/com/charlie/gallery/ui/fragment/list/ListPresenter.kt
@@ -12,17 +12,43 @@ class ListPresenter(
     override fun start() {
         view.showLoading()
         CoroutineScope(Dispatchers.IO).launch {
-            val imageItemDataList = runCatching { model.getImageList() }
-            imageItemDataList.onSuccess {
-                withContext(Dispatchers.Main) {
-                    view.hideLoading()
-                    view.showList(imageItemDataList = it)
+            runCatching { model.getImageList() }
+                .onSuccess {
+                    withContext(Dispatchers.Main) {
+                        view.hideLoading()
+                        view.showList(imageItemDataList = it)
+                    }
                 }
-            }
+                .onFailure {
+                    withContext(Dispatchers.Main) {
+                        view.hideLoading()
+                        view.showLoadingFailed()
+                    }
+                }
         }
     }
 
     override fun onClickItem(currentId: Int) {
         view.showDetailFragment(currentId = currentId)
+    }
+
+    override fun onClickReload() {
+        view.showLoading()
+        CoroutineScope(Dispatchers.IO).launch {
+            runCatching { model.getImageList() }
+                .onSuccess {
+                    withContext(Dispatchers.Main) {
+                        view.hideLoading()
+                        view.hiedLoadingFailed()
+                        view.showList(imageItemDataList = it)
+                    }
+                }
+                .onFailure {
+                    withContext(Dispatchers.Main) {
+                        view.hideLoading()
+                        view.showLoadingFailed()
+                    }
+                }
+        }
     }
 }
