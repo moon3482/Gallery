135a706857cdc1087457fc73499038cfe907da73
문창규 <ckmoon@smartscore.kr>
2023-05-23 01:30:36 +0900

    #9 BindingAdapter 추가

---
+++ app/src/main/java/com/charlie/gallery/ui/BindingAdapter.kt
@@ -0,0 +1,29 @@
+package com.charlie.gallery.ui
+
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.charlie.gallery.model.ImageItemData
+import com.charlie.gallery.ui.list.UIEvent
+import com.charlie.gallery.ui.list.adapter.ListAdapter
+
+@BindingAdapter("bind:init")
+fun initList(recyclerView: RecyclerView, imageItemDataList: List<ImageItemData>?) {
+    (recyclerView.adapter as? ListAdapter)?.initList(imageItemDataList = imageItemDataList ?: emptyList())
+}
+
+@BindingAdapter("bind:nextPage")
+fun nextPage(recyclerView: RecyclerView, imageItemDataList: List<ImageItemData>?) {
+    (recyclerView.adapter as? ListAdapter)?.addList(imageItemDataList = imageItemDataList ?: emptyList())
+}
+
+@BindingAdapter("bind:isVisible")
+fun isVisible(view: View, isVisible: Boolean) {
+    view.isVisible = isVisible
+}
+
+@BindingAdapter("bind:itemClickListener")
+fun RecyclerView.setItemClickListener(uiEvent: UIEvent) {
+    (adapter as? ListAdapter)?.setOnClickViewHolder(uiEvent::onClickItem)
+}
