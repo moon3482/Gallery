2ff22b16b56d179f4aa411a9fea640e929a2f65f
문창규 <ckmoon@smartscore.kr>
2023-05-23 01:33:50 +0900

    #9 ListFragment MVP Contract 제거 및 MVVM 코드 추가

--- app/src/main/java/com/charlie/gallery/ui/list/ListFragment.kt
+++ app/src/main/java/com/charlie/gallery/ui/list/ListFragment.kt
@@ -11,20 +11,19 @@ import androidx.fragment.app.commit
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.charlie.gallery.R
 import com.charlie.gallery.databinding.FragmentListBinding
-import com.charlie.gallery.model.ImageItemData
 import com.charlie.gallery.ui.detail.DetailFragment
 import com.charlie.gallery.ui.list.adapter.ListAdapter
 import com.charlie.gallery.ui.list.adapter.ListDecoration
 import com.charlie.gallery.util.doOnScrolled
 
-class ListFragment : Fragment(), ListContract.View {
+class ListFragment : Fragment(), UIEvent {
 
     private var _binding: FragmentListBinding? = null
     private val binding: FragmentListBinding
         get() = checkNotNull(_binding) {
             "_binding iS Null"
         }
-    private lateinit var presenter: ListContract.Presenter
+    private val listViewModel by lazy { ListViewModel(ListModel()) }
 
     // region Lifecycle
     override fun onCreateView(
@@ -33,33 +32,39 @@ class ListFragment : Fragment(), ListContract.View {
         savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentListBinding.inflate(layoutInflater)
-        presenter = ListPresenter(this, ListModel())
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
-        presenter.start()
+        observe()
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.vm = listViewModel
+        binding.event = this
     }
 
     private fun initView() {
         binding.gridListRecyclerview.apply {
-            adapter = ListAdapter { currentId ->
-                presenter.onClickItem(currentId)
-            }
+            adapter = ListAdapter()
             addItemDecoration(ListDecoration(10, 8))
             doOnScrolled { _, _, _ ->
                 val layoutManager = binding.gridListRecyclerview.layoutManager as? LinearLayoutManager
                 layoutManager?.let {
                     if (it.findLastVisibleItemPosition() == it.itemCount - 1) {
-                        presenter.onNextPage()
+                        listViewModel.onNextPage()
                     }
                 }
             }
         }
         binding.reloadButton.setOnClickListener {
-            presenter.onClickReload()
+            listViewModel.onReload()
+        }
+    }
+
+    private fun observe() {
+        listViewModel.onFailToastMessage.observe(viewLifecycleOwner) {
+            Toast.makeText(requireContext(), resources.getString(R.string.failed_load_image_list), Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -70,46 +75,14 @@ class ListFragment : Fragment(), ListContract.View {
 
     // endregion
 
-    //region ListContractor.View
-    override fun showLoading() {
-        binding.progressBar.visibility = View.VISIBLE
-    }
-
-    override fun hideLoading() {
-        binding.progressBar.visibility = View.GONE
-    }
-
-    override fun showList(imageItemDataList: List<ImageItemData>) {
-        (binding.gridListRecyclerview.adapter as? ListAdapter)?.initList(imageItemDataList)
-    }
-
-    override fun showNextPage(imageItemDataList: List<ImageItemData>) {
-        (binding.gridListRecyclerview.adapter as? ListAdapter)?.addList(imageItemDataList)
-    }
-
-    override fun showDetailFragment(currentId: Int) {
+    override fun onClickItem(position: Int) {
         parentFragmentManager.commit {
             add<DetailFragment>(
-                R.id.fragment_container,
-                args = DetailFragment.arguments(currentId)
+                R.id.fragment_container, args = DetailFragment.arguments(position)
             )
             addToBackStack(null)
         }
     }
-
-    override fun showLoadingFailed() {
-        binding.failedLoadingGroup.visibility = View.VISIBLE
-    }
-
-    override fun hiedLoadingFailed() {
-        binding.failedLoadingGroup.visibility = View.GONE
-    }
-
-    override fun showFailedToast() {
-        Toast.makeText(requireContext(), resources.getString(R.string.failed_load_image_list), Toast.LENGTH_SHORT).show()
-    }
-
-    //endregion
 
     companion object {
 
