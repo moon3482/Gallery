c176410e2fe6d59d6344217b8ce25b624ffbc128
문창규 <ckmoon@smartscore.kr>
2023-05-23 18:45:41 +0900

    #9 ListViewModel 생성자 파라미터 이름 변경

--- app/src/main/java/com/charlie/gallery/ui/list/ListViewModel.kt
+++ app/src/main/java/com/charlie/gallery/ui/list/ListViewModel.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class ListViewModel(private val model: ListModel) {
+class ListViewModel(private val model: Model) {
 
     private val _initImageList: MutableLiveData<List<ImageItemData>> = MutableLiveData()
     val initImageList: LiveData<List<ImageItemData>>
