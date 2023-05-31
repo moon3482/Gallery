c90073e5e73cc6c57ea5e2729d44d27a19459c35
문창규 <ckmoon@smartscore.kr>
2023-05-31 21:54:11 +0900

    #11 이미지 Entity 추가

---
+++ app/src/main/java/com/charlie/gallery/model/ImageEntity.kt
@@ -0,0 +1,24 @@
+package com.charlie.gallery.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tb_image")
+data class ImageEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo("row_id")
+    private val id: Long = 0,
+    @ColumnInfo(name = "id")
+    val imageId: Int,
+    @ColumnInfo(name = "author")
+    val author: String,
+    @ColumnInfo(name = "width")
+    val width: Int,
+    @ColumnInfo(name = "height")
+    val height: Int,
+    @ColumnInfo(name = "download_url")
+    val downloadUrl: String,
+    @ColumnInfo(name = "url")
+    val url: String,
+)
