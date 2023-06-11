2f003d6a9fda5aef4138a226acb088cfac1f6c95
문창규 <ckmoon@smartscore.kr>
2023-06-11 18:44:12 +0900

    #15 ListModel 수정
    - 리스트 가져오는 함수 로직 변경

--- app/src/main/java/com/charlie/gallery/ui/list/ListModel.kt
+++ app/src/main/java/com/charlie/gallery/ui/list/ListModel.kt
@@ -1,14 +1,41 @@
 package com.charlie.gallery.ui.list
 
-import com.charlie.gallery.remote.RetrofitClient
-import com.charlie.gallery.remote.model.ImageDetailDataResponse
-import retrofit2.await
+import com.charlie.gallery.local.GalleryDatabase
+import com.charlie.gallery.model.ImageDetailModel
+import com.charlie.gallery.model.ImageItemModel
+import com.charlie.gallery.remote.GalleryService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class ListModel {
-    suspend operator fun invoke(page: Int): List<ImageDetailDataResponse> {
-        return RetrofitClient
-            .galleryApi
-            .requestImageList(page = page)
-            .await()
+    private val local = GalleryDatabase.galleryDao
+    private val remote = GalleryService
+    fun getList(page: Int): Flow<List<ImageItemModel>> {
+        return flow {
+            val offset = (page - 1) * GalleryService.LIMIT
+            local
+                .getList(offset = offset, limit = GalleryService.LIMIT)
+                .map { entity -> ImageItemModel(entity) }
+                .let {
+                    emit(it)
+                }
+
+            remote.requestImageList(page = page)
+                .map {
+                    it.map { entity -> ImageDetailModel(entity) }
+                }
+                .onSuccess {
+                    local.insert(it.map { model -> model.toEntity() })
+                }
+                .map {
+                    it.map { model -> ImageItemModel(model) }
+                }
+                .fold(
+                    onSuccess = { it },
+                    onFailure = { emptyList() },
+                ).let {
+                    emit(it)
+                }
+        }
     }
 }
