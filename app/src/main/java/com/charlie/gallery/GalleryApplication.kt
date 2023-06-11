3d61d9b4d4e399fdbc74f04e61858835d21d4652
문창규 <ckmoon@smartscore.kr>
2023-06-11 18:39:36 +0900

    #15 GalleryApplication localDB init 추가가

--- app/src/main/java/com/charlie/gallery/GalleryApplication.kt
+++ app/src/main/java/com/charlie/gallery/GalleryApplication.kt
@@ -1,5 +1,12 @@
 package com.charlie.gallery
 
 import android.app.Application
+import com.charlie.gallery.local.GalleryDatabase
 
-open class GalleryApplication : Application()
+open class GalleryApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        GalleryDatabase.init(this)
+    }
+}
