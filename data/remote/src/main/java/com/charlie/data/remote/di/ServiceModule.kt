67724917b94dd3b5ff7c918c1c10fb32243ff20f
Charlie_Moon <moon3482@naver.com>
2023-07-11 22:08:05 +0900

    #17 매개변수 이름변경

--- data/remote/src/main/java/com/charlie/data/remote/di/ServiceModule.kt
+++ data/remote/src/main/java/com/charlie/data/remote/di/ServiceModule.kt
@@ -12,6 +12,6 @@ import dagger.hilt.components.SingletonComponent
 abstract class ServiceModule {
     @Binds
     abstract fun bindImageService(
-        service: ImageServiceImpl,
+        imageServiceImpl: ImageServiceImpl,
     ): ImageService
 }
