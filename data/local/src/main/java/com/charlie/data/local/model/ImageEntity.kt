a3c59d68d12d4253179e4a76c39c2a1f151f5a30
Charlie_Moon <moon3482@naver.com>
2023-07-11 17:38:33 +0900

    #17 ImageEntity ColumnInfo 수정

--- data/local/src/main/java/com/charlie/data/local/model/ImageEntity.kt
+++ data/local/src/main/java/com/charlie/data/local/model/ImageEntity.kt
@@ -7,16 +7,11 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "image")
 data class ImageEntity(
     @PrimaryKey
-    @ColumnInfo("id")
     val id: Int,
-    @ColumnInfo(name = "author")
     val author: String,
-    @ColumnInfo(name = "width")
     val width: Int,
-    @ColumnInfo(name = "height")
     val height: Int,
     @ColumnInfo(name = "download_url")
     val downloadUrl: String,
-    @ColumnInfo(name = "url")
     val url: String,
 )
