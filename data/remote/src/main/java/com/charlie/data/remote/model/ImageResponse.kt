c4225d044d54dc78fc55698ab616d777aca80675
Charlie_Moon <moon3482@naver.com>
2023-07-11 14:40:09 +0900

    #17 ImageResponse 불필요한 어노테이션 제거

--- data/remote/src/main/java/com/charlie/data/remote/model/ImageResponse.kt
+++ data/remote/src/main/java/com/charlie/data/remote/model/ImageResponse.kt
@@ -5,15 +5,10 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class ImageResponse(
-    @Json(name = "id")
     val id: Int,
-    @Json(name = "author")
     val author: String,
-    @Json(name = "width")
     val width: Long,
-    @Json(name = "height")
     val height: Long,
-    @Json(name = "url")
     val url: String,
     @Json(name = "download_url")
     val downloadUrl: String,
