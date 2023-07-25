c221b3242083120768852cca05be0483a2c696a3
Charlie_Moon <moon3482@naver.com>
2023-07-25 14:23:10 +0900

    #17 KotlinDSL 제거

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-plugins {
-    id(ANROID_APPLICATION) version ANDROID_GRADLE_VERSION apply false
-    id(ANDROID_LIBRARY) version ANDROID_GRADLE_VERSION apply false
-    id(KOTLIN) version KOTLIN_VERSION apply false
-    id(HILT) version HILT_VERSION apply false
-}
--- buildSrc/src/main/kotlin/Config.kt
+++
@@ -1,9 +0,0 @@
-import org.gradle.api.JavaVersion
-
-const val COMPIL_SDK_VERSION = 33
-const val APPLICATION_ID = "com.charlie.gallery"
-const val MIN_SDK_VERSION = 24
-const val TARGET_SDK_VERSION = COMPIL_SDK_VERSION
-const val VERSION_CODE = 1
-const val VERSION_NAME = "1.0"
-val javaVersion = JavaVersion.VERSION_11
